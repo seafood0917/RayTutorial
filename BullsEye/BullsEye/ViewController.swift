b15be7c25811d17a01bbab08500359e409730371
Daesik Choi <seafood0917@gmail.com>
2019-09-08 14:35:54 -0700

    Finished part 1.

---
+++ BullsEye/BullsEye/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Daesik on 9/8/19.
+//  Copyright © 2019 Daesik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hello, World!", message: "This is my first app!", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+}
+
